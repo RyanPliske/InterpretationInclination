b70e825ead7edb0a05d7b26b2108cafccaf9c276
Ryan Pliske <ryan.pliske@monsanto.com>
2016-11-05 14:21:14 -0500

    Initial Commit

---
+++ guessIt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  guessIt
+//
+//  Created by PLISKE, RYAN [AG-Contractor/1000] on 11/5/16.
+//  Copyright © 2016 competidev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ guessIt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  guessIt
+//
+//  Created by PLISKE, RYAN [AG-Contractor/1000] on 11/5/16.
+//  Copyright © 2016 competidev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
